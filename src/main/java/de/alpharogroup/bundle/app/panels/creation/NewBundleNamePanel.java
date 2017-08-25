34b842ee7450fe320b6970f6d585b2e961062d16
Asterios Raptis <asterios.raptis@gmx.net>
2017-08-25 17:57:54 +0200

    added callback method

--- src/main/java/de/alpharogroup/bundle/app/panels/creation/NewBundleNamePanel.java
+++ src/main/java/de/alpharogroup/bundle/app/panels/creation/NewBundleNamePanel.java
@@ -12,106 +12,108 @@ import de.alpharogroup.model.BaseModel;
 import de.alpharogroup.model.api.Model;
 import de.alpharogroup.swing.base.BasePanel;
 
-public class NewBundleNamePanel extends BasePanel<DashboardBean>
-{
+public class NewBundleNamePanel extends BasePanel<DashboardBean> {
 
-	private javax.swing.JButton btnCreateNewLocale;
-	private javax.swing.JButton btnSave;
-	private JComboBox<LanguageLocales> cmbLocale;
-	private javax.swing.JLabel lblBasename;
-	private javax.swing.JLabel lblHeaderNewBundleName;
-	private javax.swing.JLabel lblLocale;
-	private javax.swing.JTextField txtBasename;
+    private static final long serialVersionUID = 1L;
 
-	public NewBundleNamePanel()
-	{
-		this(BaseModel.<DashboardBean>of(DashboardBean.builder().build()));
-	}
+    private javax.swing.JButton btnCreateNewLocale;
+    private javax.swing.JButton btnSave;
+    private JComboBox<LanguageLocales> cmbLocale;
+    private javax.swing.JLabel lblBasename;
+    private javax.swing.JLabel lblHeaderNewBundleName;
+    private javax.swing.JLabel lblLocale;
+    private javax.swing.JTextField txtBasename;
 
-	public NewBundleNamePanel(Model<DashboardBean> model)
-	{
-		super(model);
-	}
+    public NewBundleNamePanel() {
+        this(BaseModel.<DashboardBean>of(DashboardBean.builder().build()));
+    }
 
-	@Override
-	protected void onInitializeComponents()
-	{
-		super.onInitializeComponents();
-		lblLocale = new javax.swing.JLabel();
-		cmbLocale = new javax.swing.JComboBox<>(LocaleComboBoxModel.get());
-		cmbLocale.setRenderer(new LocaleComboBoxRenderer());
-		lblBasename = new javax.swing.JLabel();
-		txtBasename = new javax.swing.JTextField();
-		btnSave = new javax.swing.JButton();
-		lblHeaderNewBundleName = new javax.swing.JLabel();
-		btnCreateNewLocale = new javax.swing.JButton();
-		btnCreateNewLocale.addActionListener(e -> onCreateNewLocale(e));
+    public NewBundleNamePanel(Model<DashboardBean> model) {
+        super(model);
+    }
 
-		lblLocale.setText("Choose Locale");
-		lblBasename.setText("Basename");
-		btnSave.setText("Save entry");
-		lblHeaderNewBundleName.setText("Create new Bundle name");
-		btnCreateNewLocale.setText("Create new custom locale");
-	}
+    @Override
+    protected void onInitializeComponents() {
+        super.onInitializeComponents();
+        lblLocale = new javax.swing.JLabel();
+        cmbLocale = new javax.swing.JComboBox<>(LocaleComboBoxModel.get());
+        cmbLocale.setRenderer(new LocaleComboBoxRenderer());
+        lblBasename = new javax.swing.JLabel();
+        txtBasename = new javax.swing.JTextField();
+        btnSave = new javax.swing.JButton();
+        btnSave.addActionListener(e -> onSave(e));
+        lblHeaderNewBundleName = new javax.swing.JLabel();
+        btnCreateNewLocale = new javax.swing.JButton();
+        btnCreateNewLocale.addActionListener(e -> onCreateNewLocale(e));
 
-	protected void onCreateNewLocale(ActionEvent e)
-	{
-	}
+        lblLocale.setText("Choose Locale");
+        lblBasename.setText("Basename");
+        btnSave.setText("Save entry");
+        lblHeaderNewBundleName.setText("Create new Bundle name");
+        btnCreateNewLocale.setText("Create new custom locale");
+    }
 
-	@Override
-	protected void onInitializeLayout()
-	{
-		super.onInitializeLayout();
+    @Override
+    protected void onInitializeLayout() {
+        super.onInitializeLayout();
 
-		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-		this.setLayout(layout);
-		layout.setHorizontalGroup(layout
-			.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-			.addGroup(layout.createSequentialGroup().addGap(40, 40, 40).addGroup(layout
-				.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-				.addGroup(layout
-					.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-					.addComponent(lblHeaderNewBundleName, javax.swing.GroupLayout.PREFERRED_SIZE,
-						720, javax.swing.GroupLayout.PREFERRED_SIZE)
-					.addGroup(layout.createSequentialGroup()
-						.addComponent(lblBasename, javax.swing.GroupLayout.PREFERRED_SIZE, 140,
-							javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addGap(18, 18, 18).addComponent(txtBasename,
-							javax.swing.GroupLayout.PREFERRED_SIZE, 562,
-							javax.swing.GroupLayout.PREFERRED_SIZE))
-					.addGroup(layout.createSequentialGroup()
-						.addComponent(lblLocale, javax.swing.GroupLayout.PREFERRED_SIZE, 140,
-							javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addGap(18, 18, 18)
-						.addComponent(cmbLocale, javax.swing.GroupLayout.PREFERRED_SIZE, 300,
-							javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED,
-							javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(btnCreateNewLocale, javax.swing.GroupLayout.PREFERRED_SIZE,
-							220, javax.swing.GroupLayout.PREFERRED_SIZE)))
-				.addComponent(btnSave, javax.swing.GroupLayout.PREFERRED_SIZE, 175,
-					javax.swing.GroupLayout.PREFERRED_SIZE))
-				.addContainerGap(40, Short.MAX_VALUE)));
-		layout
-			.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-				.addGroup(layout.createSequentialGroup().addGap(27, 27, 27)
-					.addComponent(lblHeaderNewBundleName, javax.swing.GroupLayout.PREFERRED_SIZE,
-						35, javax.swing.GroupLayout.PREFERRED_SIZE)
-					.addGap(18, 18, 18)
-					.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-						.addComponent(lblBasename)
-						.addComponent(txtBasename, javax.swing.GroupLayout.PREFERRED_SIZE,
-							javax.swing.GroupLayout.DEFAULT_SIZE,
-							javax.swing.GroupLayout.PREFERRED_SIZE))
-					.addGap(18, 18, 18)
-					.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-						.addComponent(lblLocale)
-						.addComponent(cmbLocale, javax.swing.GroupLayout.PREFERRED_SIZE,
-							javax.swing.GroupLayout.DEFAULT_SIZE,
-							javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnCreateNewLocale))
-					.addGap(18, 18, 18).addComponent(btnSave)
-					.addContainerGap(37, Short.MAX_VALUE)));
-	}
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(layout
+                .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup().addGap(40, 40, 40).addGroup(layout
+                        .createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(layout
+                                .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(lblHeaderNewBundleName, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                        720, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGroup(layout.createSequentialGroup()
+                                        .addComponent(lblBasename, javax.swing.GroupLayout.PREFERRED_SIZE, 140,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(18, 18, 18).addComponent(txtBasename,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE, 562,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGroup(layout.createSequentialGroup()
+                                        .addComponent(lblLocale, javax.swing.GroupLayout.PREFERRED_SIZE, 140,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(cmbLocale, javax.swing.GroupLayout.PREFERRED_SIZE, 300,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(btnCreateNewLocale, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                220, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(btnSave, javax.swing.GroupLayout.PREFERRED_SIZE, 175,
+                                javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(40, Short.MAX_VALUE)));
+        layout
+                .setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup().addGap(27, 27, 27)
+                                .addComponent(lblHeaderNewBundleName, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                        35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(lblBasename)
+                                        .addComponent(txtBasename, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(lblLocale)
+                                        .addComponent(cmbLocale, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(btnCreateNewLocale))
+                                .addGap(18, 18, 18).addComponent(btnSave)
+                                .addContainerGap(37, Short.MAX_VALUE)));
+    }
+
+    protected void onSave(ActionEvent e) {
+        System.out.println("de.alpharogroup.bundle.app.panels.creation.NewBundleNamePanel.onSave()");
+    }
+
+    protected void onCreateNewLocale(ActionEvent e) {
+        System.out.println("de.alpharogroup.bundle.app.panels.creation.NewBundleNamePanel.onCreateNewLocale()");
+    }
 
 }
