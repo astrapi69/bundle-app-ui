53dc7617b43ff991596a5ea3ff16e84a598bd4da
Asterios Raptis <asterios.raptis@gmx.net>
2017-09-03 22:10:53 +0200

    moved form panels to other package

--- src/test/java/de/alpharogroup/bundle/app/panels/imports/ImportBundleApplicationStartFormPanel.java
+++ src/test/java/de/alpharogroup/bundle/app/panels/imports/ImportBundleApplicationStartFormPanel.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.alpharogroup.bundle.app.panels.start;
+package de.alpharogroup.bundle.app.panels.imports;
 
 /**
  *
