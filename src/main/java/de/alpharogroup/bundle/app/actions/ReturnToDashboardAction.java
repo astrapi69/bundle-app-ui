b2236fb94c477fef4b1bd11933586600c42fa5f1
Asterios Raptis <asterios.raptis@gmx.net>
2018-02-25 11:30:17 +0100

    if model object null than go to main overview

--- src/main/java/de/alpharogroup/bundle/app/actions/ReturnToDashboardAction.java
+++ src/main/java/de/alpharogroup/bundle/app/actions/ReturnToDashboardAction.java
@@ -1,12 +1,18 @@
 package de.alpharogroup.bundle.app.actions;
 
 import java.awt.event.ActionEvent;
+import java.util.List;
 
 import javax.swing.AbstractAction;
 
 import de.alpharogroup.bundle.app.MainFrame;
 import de.alpharogroup.bundle.app.panels.dashboard.ApplicationDashboardBean;
 import de.alpharogroup.bundle.app.panels.dashboard.ApplicationDashboardContentPanel;
+import de.alpharogroup.bundle.app.panels.dashboard.mainapp.MainDashboardBean;
+import de.alpharogroup.bundle.app.panels.dashboard.mainapp.MainDashboardPanel;
+import de.alpharogroup.bundle.app.spring.SpringApplicationContext;
+import de.alpharogroup.db.resource.bundles.entities.BundleApplications;
+import de.alpharogroup.model.PropertyModel;
 import de.alpharogroup.model.api.Model;
 
 public class ReturnToDashboardAction extends AbstractAction
@@ -25,11 +31,20 @@ public class ReturnToDashboardAction extends AbstractAction
 	{
 		final Model<ApplicationDashboardBean> baModel = MainFrame.getInstance()
 			.getSelectedBundleApplicationPropertyModel();
-		final ApplicationDashboardContentPanel component = new ApplicationDashboardContentPanel(
-			baModel);
-		MainFrame.getInstance().replaceInternalFrame(
-			"Dashboard of " + baModel.getObject().getBundleApplication().getName() + " bundle app",
-			component);
+
+		if(baModel.getObject().getBundleApplication() != null) {
+			final ApplicationDashboardContentPanel component = new ApplicationDashboardContentPanel(
+				baModel);
+			MainFrame.getInstance().replaceInternalFrame(
+				"Dashboard of " + baModel.getObject().getBundleApplication().getName() + " bundle app",
+				component);
+		} else {
+			final List<BundleApplications> bundleApplications = SpringApplicationContext.getInstance()
+				.getBundleApplicationsService().findAll();
+			MainFrame.getInstance().getModelObject().setBundleApplications(bundleApplications);
+			MainFrame.getInstance().replaceInternalFrame("Overview bundle apps", new MainDashboardPanel(
+				PropertyModel.<MainDashboardBean> of(MainFrame.getInstance(), "model.object")));
+		}
 
 	}
 
